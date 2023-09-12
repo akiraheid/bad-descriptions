cb7813eccbbb332d3381a0a507049e5204ad19f2
home boy <none@ya.biz>
2023-09-12 00:24:50 -0400

    basic working website

---
+++ js/app.js
@@ -0,0 +1,27 @@
+let movies = []
+
+function getMovies() {
+	console.log("Retrieving movies")
+	$.getJSON("/movies.json", function(json) {
+		console.log(`Got data. Schema version {json.apiVersion}`)
+		movies = json.items
+		random(movies)
+	})
+}
+
+function random(list) {
+	if(!list) { list = movies }
+	const item = list[Math.floor(Math.random() * list.length)]
+	$("#description").text(item.description)
+	$("#title").text(item.title)
+	$("#title").addClass("d-none")
+	console.log(item)
+}
+
+function show() {
+	$("#title").removeClass("d-none")
+}
+
+$(document).ready(function() {
+	getMovies()
+})

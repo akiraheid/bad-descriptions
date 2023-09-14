41e1475da056b0cf0449e31d54293937ebba8039
home boy <none@ya.biz>
2023-09-14 00:08:09 -0400

    add selecting a random movie that hasn't already been seen

--- js/app.js
+++ js/app.js
@@ -1,22 +1,33 @@
 let movies = []
+let seenQueue = []
+let MAX_SEEN = 50
 
 function getMovies() {
 	console.log("Retrieving movies")
 	$.getJSON("/movies.json", function(json) {
-		console.log(`Got data. Schema version {json.apiVersion}`)
+		console.log(`Got data. Schema version ${json.apiVersion}`)
 		movies = json.items
+		if(movies.length < MAX_SEEN) {
+			MAX_SEEN = movies.length - 1
+		}
 		random(movies)
 	})
 }
 
-function random(list) {
-	if(!list) { list = movies }
-	// TODO Make sure it doesn't pick the current one
-	const item = list[Math.floor(Math.random() * list.length)]
+function random(list=movies) {
+	let item = list[Math.floor(Math.random() * list.length)]
+	while(seenQueue.some(e => e.title === item.title)) {
+		item = list[Math.floor(Math.random() * list.length)]
+	}
+
+	seenQueue.push(item)
+	if(seenQueue.length >= MAX_SEEN) {
+		seenQueue.shift()
+	}
+
 	$("#description").text(item.description)
 	$("#title").text(item.title)
 	$("#title").addClass("d-none")
-	console.log(item)
 }
 
 function show() {
